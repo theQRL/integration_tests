941e805b78a41b85cea439f861e7b6d9e7830765
Andrew Chiw <randomshinichi4869@gmail.com>
2017-11-30 04:21:38 +0000

    Rudimentary Python futures working with SendQRLToEachOther test

--- runtest_nodes_send_qrl.py
+++ runtest_nodes_send_qrl.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python
-import threading
-import time
+from concurrent.futures import ThreadPoolExecutor
 
-from qrl_testing.IntegrationTest import IntegrationTest, LogEntry
+from qrl_testing.IntegrationTest import IntegrationTest, TOTAL_NODES, LogEntry
+from qrl_testing.NodeInterface import NodeInterface
+
+pool = ThreadPoolExecutor(3)
+
 
 class SendQRLToEachOther(IntegrationTest):
+
     def __init__(self):
         super().__init__(max_running_time_secs=600)
+        self.node_state = dict()
+        self.test_running = False
 
+    @staticmethod
+    def send_qrl_test(instance):
+        """
+        Why doesn't futures work with normal methods of a class?
+        """
+        print(instance.node_state)
+        node = NodeInterface('172.19.0.3', debug=True)
+        print(str(node))
 
+    def custom_process_log_entry(self, log_entry: LogEntry):
+        if log_entry.node_id is not None:
+            self.node_state[log_entry.node_id.strip()] = log_entry.sync_state
+
+            if len(self.node_state) == TOTAL_NODES:
+                if all(s == 'synced' for s in self.node_state.values()):
+                    print("All nodes in sync! Uptime: {} secs".format(self.running_time))
+                    # future = pool.submit(self.writeout, "WTF")
+                    if not self.test_running:
+                        pool.submit(self.send_qrl_test, self)
+                        self.test_running = True
 if __name__ == '__main__':
     test = SendQRLToEachOther()
     test.start()
