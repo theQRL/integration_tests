66df207b1f271b70765f70ff95d99370a05b391a
Andrew Chiw <randomshinichi4869@gmail.com>
2017-11-30 06:44:38 +0000

    SendQRLToEachOther collecting information about the nodes

--- runtest_nodes_send_qrl.py
+++ runtest_nodes_send_qrl.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
+import os
+
 from concurrent.futures import ThreadPoolExecutor
 
 from qrl_testing.IntegrationTest import IntegrationTest, TOTAL_NODES, LogEntry
 from qrl_testing.NodeInterface import NodeInterface
+
+# This code depends on a modded qrl.core. Set PYTHONPATH.
+from qrl.core.Transaction import Transaction
+from qrl.core.Wallet import Wallet, AddressBundle
 
 pool = ThreadPoolExecutor(3)
 
@@ -11,7 +17,6 @@ class SendQRLToEachOther(IntegrationTest):
 
     def __init__(self):
         super().__init__(max_running_time_secs=600)
-        self.node_state = dict()
         self.test_running = False
 
     @staticmethod
@@ -19,9 +24,27 @@ class SendQRLToEachOther(IntegrationTest):
         """
         Why doesn't futures work with normal methods of a class?
         """
-        IntegrationTest.writeout("FUUUUUUCK")
-        node = NodeInterface('172.19.0.3', debug=True)
-        print(str(node))
+        IntegrationTest.writeout("GATHERING INFORMATION ABOUT NODES")
+        for node_id in instance.node_state:
+            volumes_dir = "volumes/testsintegration_{}".format(node_id)
+            wallet_dir = os.path.join(volumes_dir, "wallet/")
+            wallet_address_file = os.path.join(volumes_dir, "wallet_address")
+            ip_file = os.path.join(volumes_dir, "node_ip")
+
+            with open(wallet_address_file) as f:
+                instance.node_state[node_id].Qaddress = f.readline().strip()
+            with open(ip_file) as f:
+                instance.node_state[node_id].ip = f.readline().strip()
+            instance.node_state[node_id].wallet_dir = wallet_dir
+
+        IntegrationTest.writeout(instance.node_state)
+
+        node_1 = instance.node_state["node_1"]
+        node = NodeInterface(node_1.ip, debug=True)
+        node_1_wallet = Wallet(wallet_path="/home/shinichi/qrlwallet")
+        IntegrationTest.writeout(node_1_wallet)
+        node_2_wallet = Wallet(wallet_path=node_2.wallet_dir)
+        # return True
 
     def custom_process_log_entry(self, log_entry: LogEntry):
         if log_entry.node_id is not None:
@@ -33,6 +56,7 @@ class SendQRLToEachOther(IntegrationTest):
                     if not self.test_running:
                         pool.submit(self.send_qrl_test, self)
                         self.test_running = True
+
 if __name__ == '__main__':
     test = SendQRLToEachOther()
     test.start()
