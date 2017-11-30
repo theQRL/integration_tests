7aa604e7bbd1d3a8f7d311300e0bd08694deef47
Andrew Chiw <randomshinichi4869@gmail.com>
2017-11-30 21:08:53 +0000

    Send QRL test! it fails...

--- runtest_nodes_send_qrl.py
+++ runtest_nodes_send_qrl.py
@@ -7,7 +7,6 @@ from concurrent.futures import ThreadPoolExecutor
 from qrl_testing.IntegrationTest import IntegrationTest, TOTAL_NODES, LogEntry
 from qrl_testing.NodeInterface import NodeInterface
 
-# This code depends on a modded qrl.core. Set PYTHONPATH.
 from qrl.core.Transaction import Transaction
 from qrl.core.Wallet import Wallet, AddressBundle
 from qrl.core import config
@@ -26,48 +25,41 @@ class SendQRLToEachOther(IntegrationTest):
         """
         Why doesn't futures work with normal methods of a class?
         """
-        IntegrationTest.writeout("GATHERING INFORMATION ABOUT NODES")
-        for node_id in instance.node_state:
-            volumes_dir = "volumes/testsintegration_{}".format(node_id)
-            wallet_dir = os.path.join(volumes_dir, "wallet/")
-            wallet_address_file = os.path.join(volumes_dir, "wallet_address")
-            ip_file = os.path.join(volumes_dir, "node_ip")
+        for s in instance.node_states.values():
+            s.find_ip_Qaddress_wallet()
 
-            with open(wallet_address_file) as f:
-                instance.node_state[node_id].Qaddress = f.readline().strip()
-            with open(ip_file) as f:
-                instance.node_state[node_id].ip = f.readline().strip()
-            instance.node_state[node_id].wallet_dir = wallet_dir
-
-        node_1 = instance.node_state["node_1"]
-        node_2 = instance.node_state["node_2"]
+        node_1 = instance.node_states["node_1"]
+        node_2 = instance.node_states["node_2"]
         node = NodeInterface(node_1.ip, debug=True)
-
-        IntegrationTest.writeout("YOU ARE ENTERING SEND_QRL_TEST")
         
         # This is like saying Wallet(wallet_dir)
         config.user.wallet_path = node_1.wallet_dir
         node_1_wallet = Wallet()
 
-        response = node.send(from_addr=node_1_wallet.address_bundle[0], to_addr=node_2.Qaddress.encode(), amount=10, fee=1)
-        time.sleep(5)
-        node_2_balance = node.check_balance(address=node_2_wallet.addresses)
-        IntegrationTest.writeout(node_2_balance)
+        # Because all nodes stake in the integration test network, we can't send txns
+        # from them. So we need to create a second AddressBundle to send money from.
+        IntegrationTest.writeout("GENERATING SECOND ADDRESS TO SEND QRL FROM")
+        node_1_wallet.address_bundle.append(Wallet.get_new_address())
 
-        # return True
+        response = node.send(from_addr=node_1_wallet.address_bundle[1], to_addr=node_2.Qaddress.encode(), amount=10, fee=1)
+        time.sleep(5)
+        node_2_balance = node.check_balance(address=node_2.Qaddress.encode())
+        IntegrationTest.writeout(node_2_balance)
+        if node_2_balance > 100000000000000:
+            instance.successful_test()
+        else:
+            instance.fail_test()
+
 
     def custom_process_log_entry(self, log_entry: LogEntry):
         if log_entry.node_id is not None:
-            self.update_node_synced(log_entry.node_id, log_entry.synced)
-            self.update_node_grpc_started(log_entry.node_id, log_entry.rest)
+            self.update_node_state(log_entry.node_id, log_entry)
 
             if self.all_nodes_synced:
-                    print("All nodes in sync! Uptime: {} secs".format(self.running_time))
-                    # future = pool.submit(self.writeout, "WTF")
-                    if self.all_nodes_grpc_started and not self.test_running:
-                        print("GRPC READY ON ALL NODES, CONDUCTING TEST")
-                        pool.submit(self.send_qrl_test, self)
-                        self.test_running = True
+                print("All nodes in sync! Uptime: {} secs".format(self.running_time))
+                if self.all_nodes_grpc_started and not self.test_running:
+                    pool.submit(self.send_qrl_test, self)
+                    self.test_running = True
 
 if __name__ == '__main__':
     test = SendQRLToEachOther()
