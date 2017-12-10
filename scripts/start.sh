ff8069513de3c775988f1fde5a0ec83548dfd5cf
Juan Leni <lenijuan@gmail.com>
2017-12-10 20:17:29 +0100

    Improvement to integration test infrastructure

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,20 +1,21 @@
 #!/bin/bash -u
 
-groupadd -g $DOCKER_GID testuser
-useradd -u $DOCKER_UID -g $DOCKER_GID testuser
 export USERNAME="testuser"
+id -g $DOCKER_GID > /dev/null || groupadd -g $DOCKER_GID $USERNAME
+id -u $DOCKER_UID > /dev/null || useradd -u $DOCKER_UID -g $DOCKER_GID $USERNAME
 
 mkdir -p /home/${USERNAME}
 
 # Get volume name based on container name
-EASYNAME=$(python3 /root/scripts/get_name.py)
+EASYNAME=$(python3 /root/scripts/docker_helper.py)
 echo "EasyName: ${EASYNAME}"
-VOLUME_NAME="/volumes${EASYNAME}"
+
+VOLUME_NAME="/volumes/${EASYNAME}"
 
 # Copy scripts and configuration
 cp /root/scripts/run_user.sh  /home/${USERNAME}/run_user.sh
 mkdir -p ${VOLUME_NAME}
-ln -s ${VOLUME_NAME} /home/${USERNAME}/.qrl
+ln -sf ${VOLUME_NAME} /home/${USERNAME}/.qrl
 
 if [[ -v LOCALNET_ONLY ]]; then
     echo "Restricting to LOCALNET"
@@ -28,4 +29,8 @@ chown -R ${USERNAME}:${USERNAME} ${VOLUME_NAME}
 chmod -R a+rwx /home/${USERNAME}
 
 # Launch user script
-sudo BOOT_PHASE=${BOOT_PHASE} INTEGRATION_TESTINPLACE=${INTEGRATION_TESTINPLACE} REPO_SLUG=${REPO_SLUG} REPO_BRANCH=${REPO_BRANCH} EASYNAME=${EASYNAME} -i -u ${USERNAME} /home/${USERNAME}/run_user.sh
+sudo BOOT_PHASE=${BOOT_PHASE} \
+     INTEGRATION_TESTINPLACE=${INTEGRATION_TESTINPLACE} \
+     REPO_SLUG=${REPO_SLUG} REPO_BRANCH=${REPO_BRANCH} \
+     EASYNAME=${EASYNAME} -i -u ${USERNAME} \
+     /home/${USERNAME}/run_user.sh
