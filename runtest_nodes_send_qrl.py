56df3df182dea4c6e991fa8b362c299f11990d64
Andrew Chiw <randomshinichi4869@gmail.com>
2017-12-16 13:39:56 +0100

    send QRL test - checks src and dst address balances. Uses a simple 2min wait for tx to confirm.

--- runtest_nodes_send_qrl.py
+++ runtest_nodes_send_qrl.py
@@ -1,64 +1,118 @@
 #!/usr/bin/env python
+import json
 import os
-import time
-
+import subprocess
+from time import sleep
 from concurrent.futures import ThreadPoolExecutor
 
-from qrl_testing.IntegrationTest import IntegrationTest, TOTAL_NODES, LogEntry
-from qrl_testing.NodeInterface import NodeInterface
+from qrl_testing.IntegrationTest import IntegrationTest, LogEntry
 
-from qrl.core.Transaction import Transaction
-from qrl.core.Wallet import Wallet, AddressBundle
-from qrl.core import config
-
-pool = ThreadPoolExecutor(3)
+pool = ThreadPoolExecutor(1)
 
 
 class SendQRLToEachOther(IntegrationTest):
 
-    def __init__(self):
-        super().__init__(max_running_time_secs=600)
+    def __init__(self, max_running_time_secs=None):
+        super().__init__(max_running_time_secs=max_running_time_secs)
         self.test_running = False
+        self.test_successful = None
 
     def send_qrl_test(self):
+        def read_machine_decodable_output(o: str):
+            output = json.loads(o)
+            location = output["location"]
+            addr = output["addresses"][0]["address"]
+            bal = output["addresses"][0]["balance"]
+            return (location, addr, bal)
+
+        def wallet_gen(wallet_dir, ip):
+            """
+            :param wallet_dir: volumes/srcwallet; volumes/dstwallet
+            :param ip: 127.0.0.1
+            :return: ('volumes/srcwallet', 'Q...', 100)
+            """
+            IntegrationTest.writeout("Generating 3rd party {} wallet".format(wallet_dir))
+            tempwallet_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), wallet_dir)
+            gen_cmd = subprocess.Popen(["qrl", "--wallet_dir", tempwallet_dir, "--host", ip, "-m", "wallet_gen"],
+                                       stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            gen_cmd.wait(timeout=10)
+            return read_machine_decodable_output(gen_cmd.stdout.read().decode())
+
+        def wallet_ls(wallet_dir, ip):
+            """
+            :param wallet_dir: volumes/srcwallet; volumes/dstwallet
+            :param ip: 127.0.0.1
+            :return: ('volumes/srcwallet', 'Q...', 100)
+            """
+            tempwallet_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), wallet_dir)
+            ls_cmd = subprocess.Popen(
+                ["qrl", "--wallet_dir", tempwallet_dir, "--host", ip, "-m", "wallet_ls"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT
+            )
+            ls_cmd.wait(timeout=10)
+            return read_machine_decodable_output(ls_cmd.stdout.read().decode())
+
         IntegrationTest.writeout("Beginning Send QRL Integration Test")
 
         for s in self.node_states.values():
             s.find_ip_Qaddress_wallet()
         node_1 = self.node_states["node_1"]
         node_2 = self.node_states["node_2"]
-        node = NodeInterface(node_1.ip, debug=True)
 
-        # This is like saying Wallet(wallet_dir)
-        config.user.wallet_path = node_1.wallet_dir
-        node_1_wallet = Wallet()
+        src_loc, src_addr, src_bal_old = wallet_gen('volumes/srcwallet', node_1.ip)
+        dst_loc, dst_addr, dst_bal_old = wallet_gen('volumes/dstwallet', node_1.ip)
 
-        # Because all nodes stake in the integration test network, we can't send txns
-        # from them. So we need to create a second AddressBundle to send money from.
-        IntegrationTest.writeout("GENERATING SECOND ADDRESS TO SEND QRL FROM")
-        node_1_wallet.address_bundle.append(Wallet.get_new_address())
+        amount = 50
+        fee = 13
+        IntegrationTest.writeout("qrl tx_transfer {},{}".format(amount, fee))
+        tx_transfer = subprocess.Popen(
+            ["qrl", "--wallet_dir", src_loc, "-r", "--host", node_1.ip, "tx_transfer", "--src",
+             src_addr, "--dst", dst_addr, "--amount", str(amount), "--fee", str(fee)],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT
+        )
+        tx_transfer.wait(timeout=10)
+        resp = tx_transfer.stdout.read().decode()
+        if 'True' not in resp:
+            IntegrationTest.writeout("qrl tx_transfer error: {}".format(resp))
+            self.test_successful = False
+            return
 
-        sending_address_new_balance = node.check_balance(address=sending_address)
-        receiving_address_new_balance = node.check_balance(address=receiving_address)
-        IntegrationTest.writeout(
-            "Sending address now has: {}, Receiving address now has: {}".format(sending_address_new_balance,
-                                                                                receiving_address_new_balance))
-        if receiving_address_new_balance == (receiving_address_old_balance + 10):
-            self.successful_test()
+        IntegrationTest.writeout("TX sent. Waiting a bit...")
+        sleep(120)
+
+        _, _, src_bal_new = wallet_ls('volumes/srcwallet', node_2.ip)
+        _, _, dst_bal_new = wallet_ls('volumes/dstwallet', node_2.ip)
+        IntegrationTest.writeout("src balance {}->{}".format(src_bal_old, src_bal_new))
+        IntegrationTest.writeout("dst balance {}->{}".format(dst_bal_old, dst_bal_new))
+
+        if (src_bal_new == src_bal_old - (amount + fee)) and (dst_bal_new == (dst_bal_old + amount)):
+            self.test_successful = True
         else:
-            self.fail_test()
+            self.test_successful = False
+
 
 
     def custom_process_log_entry(self, log_entry: LogEntry):
         if log_entry.node_id is not None:
             self.update_node_state(log_entry.node_id, log_entry)
 
-            if self.all_nodes_synced:
-                print("All nodes in sync! Uptime: {} secs".format(self.running_time))
-                if self.all_nodes_grpc_started and not self.test_running:
+            if self.all_nodes_synced and self.all_nodes_grpc_started:
+                if not self.test_running:
+                    print("Uptime: {} secs, all nodes in sync and gRPC servers running! Starting test".format(
+                        self.running_time))
                     pool.submit(self.send_qrl_test)
                     self.test_running = True
 
+            if self.test_running:
+                if self.test_successful is True:
+                    self.successful_test()
+                elif self.test_successful is False:
+                    self.fail_test()
+
+
+
 if __name__ == '__main__':
-    test = SendQRLToEachOther()
+    test = SendQRLToEachOther(max_running_time_secs=None)
     test.start()
