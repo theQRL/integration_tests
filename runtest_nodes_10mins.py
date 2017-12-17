21dc82b189887da17006140485f9283acab9ce61
Andrew Chiw <randomshinichi4869@gmail.com>
2017-12-17 11:07:49 +0100

    Travis CI should run send_qrl test. reenable other integration tests No need to compile CMake, Travis already has a recent version in their images. send_qrl_test max_running_time reinstated runtest_nodes_10mins, runtest_nodes_1minAfterSync updated to work with new IntegrationTest.LogEntry

--- runtest_nodes_10mins.py
+++ runtest_nodes_10mins.py
@@ -14,7 +14,7 @@ class RunFor10Minutes(IntegrationTest):
 
     def custom_process_log_entry(self, log_entry: LogEntry):
         if log_entry.node_id is not None:
-            self.node_state[log_entry.node_id] = log_entry.sync_state
+            self.node_state[log_entry.node_id] = log_entry.sync_status
             if len(self.node_state) == TOTAL_NODES and not self.could_sync:
                 if all(s == 'synced' for s in self.node_state.values()):
                     IntegrationTest.writeout("******************** NODES SYNCED ********************")
