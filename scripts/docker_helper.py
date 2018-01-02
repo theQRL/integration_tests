39dbfca59ae0fc607e305d33fda3c339e6c39633
Andrew Chiw <randomshinichi4869@gmail.com>
2018-01-02 15:28:52 +0100

    If you can't find a container, raise an Exception

--- scripts/docker_helper.py
+++ scripts/docker_helper.py
@@ -16,17 +16,24 @@ def get_main_ip(container):
 
 def get_container_from_name(container_name):
     client = docker.from_env()
-    for cont in client.containers.list():
-        if cont.name.startswith(container_name):
+    containers = client.containers.list()
+    for cont in containers:
+        if container_name in cont.name:
             return cont
+
+    names = ', '.join([cont.name for cont in containers])
+    raise Exception("couldn't find {} - valid names are {}".format(container_name, names))
 
 
 def get_container_from_hash(container_hash):
     client = docker.from_env()
-    for cont in client.containers.list():
+    containers = client.containers.list()
+    for cont in containers:
         if cont.id.startswith(container_hash):
             return cont
 
+    ids = ', '.join([cont.id for cont in containers])
+    raise Exception("couldn't find {} - valid hashes are {}".format(container_hash, ids))
 
 if __name__ == '__main__':
     hostname = socket.gethostname()
