ced94c201052f1dea2896c12d44db713b39a0e09
Andrew Chiw <randomshinichi4869@gmail.com>
2018-03-06 16:44:44 +0100

    TestLogParser, CheckNodesSynchronize: don't shout "SUCCESS" when synced

--- tests/helpers/nodes_synchronize.py
+++ tests/helpers/nodes_synchronize.py
@@ -13,9 +13,7 @@ class CheckNodesSynchronize(TestLogParser):
             self.node_state[log_entry.node_id] = log_entry.sync_state
             if len(self.node_state) == TOTAL_NODES:
                 if all(s == 'synced' for s in self.node_state.values()):
-                    print("All nodes in sync! Uptime: {} secs".format(self.running_time))
                     self.sync_event.set()
-                    return self.successful_test()
 
 
 def wait_for_sync(sync_event):
