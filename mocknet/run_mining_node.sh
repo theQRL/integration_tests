2202aa1ba6c8176298dae275af9ed57f00a1072b
Scott Donald <scottdonald@protonmail.ch>
2018-05-11 06:13:41 +1000

    Additional script specific for mining tests

--- mocknet/run_mining_node.sh
+++ mocknet/run_mining_node.sh
@@ -7,5 +7,5 @@ source ${BASH_SOURCE%/*}/set_env.sh
 echo
 source ${VENV_PATH}/bin/activate
 export PYTHONPATH=${SOURCE_PATH}/src
-${SOURCE_PATH}/start_qrl.py -l DEBUG "$@"
+${SOURCE_PATH}/start_qrl.py --mocknet -l DEBUG "$@"
 
