8a371be6efd58a804c56ddd39f2b492f10162535
Juan Leni <lenijuan@gmail.com>
2018-06-01 00:51:36 +0200

    Adding 2 min fastnet test

---
+++ tests/python/fastnet/test_2min.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+# Distributed under the MIT software license, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+from unittest import TestCase
+
+from mocknet.MockNet import MockNet
+from mocknet.NodeTracker import NodeLogTracker
+
+
+class TestMocknet10Min(TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def test_launch_log_nodes(self):
+        def func_monitor_log():
+            node_logtracker = NodeLogTracker(mocknet)
+
+            while mocknet.uptime < 120:
+                node_logtracker.track()
+
+        mocknet = MockNet(func_monitor_log,
+                          timeout_secs=150,
+                          node_count=3,
+                          node_args="--mocknet")
+
+        mocknet.prepare_source()
+        mocknet.run()
