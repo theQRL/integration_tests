07bdd809716f071256bc7c437f013897d46bbc5f
cyyber <kaushal.forex@gmail.com>
2018-06-11 16:58:45 +0530

    Fix: Fork Recovery test

--- tests/python/fork_recovery/scripts/prepare_data.sh
+++ tests/python/fork_recovery/scripts/prepare_data.sh
@@ -15,5 +15,7 @@ rm -rf ${PROJECT_PATH}/tmp/data/node000
 rm -rf ${PROJECT_PATH}/tmp/data/node001
 
 mkdir -p ${PROJECT_PATH}/tmp/data/node000
-mv ${PROJECT_PATH}/tmp/testing_data/fork_recovery/.qrl_150 ${PROJECT_PATH}/tmp/data/node000
-mv ${PROJECT_PATH}/tmp/testing_data/fork_recovery/.qrl_200 ${PROJECT_PATH}/tmp/data/node001
+mv ${PROJECT_PATH}/tmp/testing_data/fork_recovery/.qrl_150/data ${PROJECT_PATH}/tmp/data/node000
+
+mkdir -p ${PROJECT_PATH}/tmp/data/node001
+mv ${PROJECT_PATH}/tmp/testing_data/fork_recovery/.qrl_200/data ${PROJECT_PATH}/tmp/data/node001
