88797cf709b765e48148b4740ac8f646567ad140
cyyber <kaushal.forex@gmail.com>
2018-06-20 02:23:47 +0530

    Implemented State Validator and added into Fork Recovery Test

--- tests/python/fork_recovery/test_fork_recovery.py
+++ tests/python/fork_recovery/test_fork_recovery.py
@@ -3,15 +3,12 @@
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 import os
 import subprocess
-import grpc
 from unittest import TestCase
 
 from mocknet.MockNet import MockNet
 from mocknet.NodeTracker import NodeLogTracker
 
-from pyqrllib.pyqrllib import hstr2bin, bin2hstr
-
-from qrl.generated import qrl_pb2_grpc, qrl_pb2
+from validators.StateValidator import StateValidator
 
 TIMEOUT = 240
 LAST_BLOCK_NUMBER = 201
@@ -51,40 +48,12 @@ class TestMocknetForkRecovery(TestCase):
                             len(address_state.transaction_hashes))
 
     def test_fork_recovery(self):
-        def state_check():
-            public_api_addresses = mocknet.public_addresses
-            for public_api_address in public_api_addresses:
-                channel_public = grpc.insecure_channel(public_api_address)
-                stub = qrl_pb2_grpc.PublicAPIStub(channel_public)
-
-                # TODO: Check coins emitted, coins total supply, epoch, block_last_reward
-                # response = stub.GetStats(request=qrl_pb2.GetStatsReq())
-
-                response = stub.GetNodeState(request=qrl_pb2.GetNodeStateReq())
-                if response.info.block_height != LAST_BLOCK_NUMBER:
-                    raise Exception('Expected Blockheight %s \n Found blockheight %s',
-                                    LAST_BLOCK_NUMBER,
-                                    response.info.block_height)
-
-                if response.info.block_last_hash != bytes(hstr2bin(LAST_BLOCK_HEADERHASH)):
-                    raise Exception('Last Block Headerhash mismatch\n'
-                                    'Expected : %s\n', bin2hstr(response.info.block_last_hash),
-                                    'Found : %s ', LAST_BLOCK_HEADERHASH)
-
-                response = stub.GetAddressState(request=qrl_pb2.GetAddressStateReq(address=bytes(hstr2bin(ADDR_1))))
-                self._verify_address_state(response.state, BALANCE_1, TX_COUNT_1)
-
-                response = stub.GetAddressState(request=qrl_pb2.GetAddressStateReq(address=bytes(hstr2bin(ADDR_2))))
-                self._verify_address_state(response.state, BALANCE_2, TX_COUNT_2)
-
-            return True
-
         def func_monitor_log():
             node_tracker = NodeLogTracker(mocknet)
             while mocknet.running:
                 msg = node_tracker.track()
                 if "Added Block #{0} {1}".format(LAST_BLOCK_NUMBER, LAST_BLOCK_HEADERHASH) in msg:
-                    state_check()
+                    StateValidator(mocknet.debug_addresses).validate_state()
                     return
 
         mocknet = MockNet(func_monitor_log,
