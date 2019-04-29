277e38642baef5a612afe190fc34846dab9ffc15
JP Lomas <jp@theqrl.org>
2019-04-29 17:26:16 +0100

    Update test.js

--- tests/js/test.js
+++ tests/js/test.js
@@ -621,8 +621,8 @@ describe('GetLatestData - All', function() {
         response.blockheaders.forEach((i) => {
             console.log(`block_number: ${i.header.block_number}`);
             console.log(`hash_header_prev: ${i.header.hash_header_prev}`);
-            expect(i.header.hash_header_prev.length).to.equal(32) );
-        }
+            expect(i.header.hash_header_prev.length).to.equal(32);
+        })
         response.blockheaders.forEach(i => expect( Buffer.isBuffer(i.header.merkle_root)).to.equal(true) );
         response.blockheaders.forEach(i => expect(i.header.merkle_root.length).to.equal(32) );
     });
