b3ad11212c7914555a95b69ec72dda2445a09276
JP Lomas <jp@theqrl.org>
2019-04-29 16:52:28 +0100

    Update test.js

--- tests/js/test.js
+++ tests/js/test.js
@@ -593,7 +593,6 @@ describe('GetLatestData - All', function() {
                         return;
                     }
                     response = res;
-                    console.log(res);
                     resolve();
                 });
             });
@@ -619,7 +618,11 @@ describe('GetLatestData - All', function() {
         response.blockheaders.forEach(i => expect(i.header.extra_nonce).to.be.a('string') );
         response.blockheaders.forEach(i => expect(parseInt(i.header.extra_nonce)).to.be.a('number') );
         response.blockheaders.forEach(i => expect( Buffer.isBuffer(i.header.hash_header_prev)).to.equal(true) );
-        response.blockheaders.forEach(i => expect(i.header.hash_header_prev.length).to.equal(32) );
+        response.blockheaders.forEach((i) => {
+            console.log(`block_number: ${i.header.block_number}`);
+            console.log(`hash_header_prev: ${i.header.hash_header_prev}`);
+            expect(i.header.hash_header_prev.length).to.equal(32) );
+        }
         response.blockheaders.forEach(i => expect( Buffer.isBuffer(i.header.merkle_root)).to.equal(true) );
         response.blockheaders.forEach(i => expect(i.header.merkle_root.length).to.equal(32) );
     });
